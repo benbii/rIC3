4d994b230dbe6cbd6cfc890d4a55fbad4c7d19cd
benbii <alittlesheep777@gmail.com>
2026-05-12 22:22:59 +0800

    maybe somewhat ABC equivalent now

--- src/transys/toy-scorr.rs
+++ src/transys/toy-scorr.rs
@@ -1,15 +1,17 @@
 use crate::{
-    Lit, aig::Aig, frontend::{Frontend, aig::AigFrontend},
+    aig::Aig,
+    frontend::{aig::AigFrontend, Frontend},
     gipsat::DagCnfSolver,
-    transys::{Transys, certify::Restore}
+    transys::{certify::Restore, Transys},
+    Lit,
 };
 use ahash::HashMap;
 use logicrs::bitvec::BitVec;
-use logicrs::{LitVec, Var, VarLMap, VarMap, satif::Satif};
-use rand::{SeedableRng, rngs::StdRng, seq::SliceRandom};
+use logicrs::{satif::Satif, LitVec, Var, VarLMap, VarMap};
+use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 use std::{iter::zip, path::PathBuf};
 
-pub fn toy_scorr(model:PathBuf, _output: PathBuf) -> (Transys, Restore) {
+pub fn toy_scorr(model: PathBuf, _output: PathBuf) -> (Transys, Restore) {
     let model = model.canonicalize().unwrap();
     let mut ts = AigFrontend::new(Aig::from_file(&model)).ts();
     println!("original ts: {}", ts.statistic());
@@ -46,10 +48,19 @@ pub fn toy_scorr(model:PathBuf, _output: PathBuf) -> (Transys, Restore) {
     let mut rng = StdRng::seed_from_u64(123456789);
     let mut maybe = ts.latch.clone();
     maybe.sort(); // so that small ID becomes class representitive
-    'sweep: loop {
+    let mut replace = VarLMap::new();
+    let mut prevround_simsz = 0;
+    // A SAT pushing sim trace length **will always** split classes further because in the trace
+    // x != y, causing the class containing x and y to split, so progress are always made
+    while prevround_simsz != sim[Var::CONST].len() {
+        prevround_simsz = sim[Var::CONST].len();
+
         // 1: Extract equivalence classes
         let mut cand: HashMap<BitVec, LitVec> = HashMap::default();
-        cand.insert(sim[Var::CONST].clone(), LitVec::from([Lit::constant(false)]));
+        cand.insert(
+            sim[Var::CONST].clone(),
+            LitVec::from([Lit::constant(false)]),
+        );
         for &v in maybe.iter() {
             let l = v.lit();
             if let Some(c) = cand.get_mut(&sim[v]) {
@@ -60,7 +71,7 @@ pub fn toy_scorr(model:PathBuf, _output: PathBuf) -> (Transys, Restore) {
                 cand.insert(sim[v].clone(), LitVec::from([l]));
             }
         }
-        let mut replace = VarLMap::new();
+        replace.clear();
         cand.retain(|_, eqc| {
             let repr = eqc[0];
             if eqc.len() <= 1 {
@@ -76,8 +87,7 @@ pub fn toy_scorr(model:PathBuf, _output: PathBuf) -> (Transys, Restore) {
         });
         maybe.retain(|v| !sim[*v].is_empty());
 
-        // 2 THE QUERY: on replaced transition, try [xn, !yn] and [!yn, xn],
-        // and acquire next-state latches.
+        // 2: build solver
         let mut rel = ts.rel.clone();
         rel.replace(&replace);
         let mut slv = DagCnfSolver::new(&rel);
@@ -86,47 +96,54 @@ pub fn toy_scorr(model:PathBuf, _output: PathBuf) -> (Transys, Restore) {
             slv.add_clause(&[c]);
         }
         slv.rng = rng;
+        slv.use_phase_saving = false;
         let nxt_maybe: Vec<Lit> = maybe.iter().map(|c| ts.next(c.lit())).collect();
-        let domain: Vec<Var> = nxt_maybe.iter()
+        let domain: Vec<Var> = nxt_maybe
+            .iter()
             .map(|l| replace.map_lit(*l).unwrap_or(*l).var())
             .collect();
-        println!("built base solver with {} eqv classes", cand.len());
+        println!("{prevround_simsz} patterns, {} eqv classes", cand.len());
 
         // 3: sweep on x != y
         for eqc in cand.values() {
             let x = eqc[0];
             for &y in eqc.iter().skip(1) {
                 // 4: Extract one total current-frame witness from the quotiented SAT model.
+                // If in current outer loop round some SAT traces already discern the two, just stop
+                // checking them. The comparison is incomplete, but 64~128 traces is decent enough.
+                if sim[x.var()].ne_coarse(&sim[y.var()], x.polarity() != y.polarity()) {
+                    continue;
+                }
                 let xn = if x.var().is_constant() { x } else { ts.next(x) };
                 // `y` comes from `eqc.iter().skip(1)`, so it should not be constant.
                 let yn = ts.next(y);
-                // Yes. `rel.replace(&replace)` only rewrites occurrences inside the relation.
-                // The top-level query literals still need to be mapped explicitly as well.
                 let xn = replace.map_lit(xn).unwrap_or(xn);
                 let yn = replace.map_lit(yn).unwrap_or(yn);
-                if slv.solve_with_domain(&LitVec::from([xn, !yn]), &domain)
-                    || slv.solve_with_domain(&LitVec::from([!xn, yn]), &domain)
+                if !slv.solve_with_domain(&LitVec::from([xn, !yn]), &domain)
+                    && !slv.solve_with_domain(&LitVec::from([!xn, yn]), &domain)
                 {
-                    sim[Var::CONST].push(false);
-                    for (y, yn) in zip(maybe.iter(), nxt_maybe.iter()) {
-                        let yn = replace.map_lit(*yn).unwrap_or(*yn);
-                        sim[*y].push(slv.sat_value(yn).unwrap());
-                    }
-                    rng = slv.rng;
-                    continue 'sweep;
+                    continue; // x == y under current assumption :D
+                }
+                sim[Var::CONST].push(false);
+                for (l, ln) in zip(maybe.iter(), nxt_maybe.iter()) {
+                    let ln = replace.map_lit(*ln).unwrap_or(*ln);
+                    sim[*l].push(slv.sat_value(ln).unwrap());
                 }
             }
         }
 
-        ts.replace(&replace, &mut rst);
-        return (ts, rst);
+        rng = slv.rng; // so that solver rng does not restart with 0
     }
+    ts.replace(&replace, &mut rst);
+    return (ts, rst);
 }
 
 #[allow(dead_code)]
 fn rt_simulation(ts: &Transys, sim: &mut VarMap<BitVec>, nr_patt: usize) {
     fn assign(sim: &VarMap<BitVec>, idx: usize, vars: &[Var]) -> LitVec {
-        vars.iter().map(|&v| v.lit().not_if(!sim[v].get(idx))).collect()
+        vars.iter()
+            .map(|&v| v.lit().not_if(!sim[v].get(idx)))
+            .collect()
     }
     let consider: Vec<_> = ts.latch().filter(|v| !sim[*v].is_empty()).collect();
     // HELP: let domain: Vec<_> = consider.iter().map(|&v| ts.next(v.lit()).var()).collect();
@@ -134,7 +151,9 @@ fn rt_simulation(ts: &Transys, sim: &mut VarMap<BitVec>, nr_patt: usize) {
     let init_len = sim[Var::CONST].len();
     let mut slv = DagCnfSolver::new(&ts.rel);
     slv.use_phase_saving = false;
-    for cls in ts.constraint() { slv.add_clause(&cls.cube()); }
+    for cls in ts.constraint() {
+        slv.add_clause(&cls.cube());
+    }
     for i in 0..init_len {
         let block = !assign(sim, i, &consider);
         let block = ts.lits_next(block.iter());
@@ -151,7 +170,9 @@ fn rt_simulation(ts: &Transys, sim: &mut VarMap<BitVec>, nr_patt: usize) {
         from: usize,
     ) {
         let assump = assign(sim, from, consider);
-        if sim[Var::CONST].len() >= nr_patt { return; }
+        if sim[Var::CONST].len() >= nr_patt {
+            return;
+        }
         // Some(5) limit is usually hit deep inside DFS when most assignments
         // in current exploration is blocked. At this time, better get off
         // and try other, shallower pre-image patterns :D
@@ -171,11 +192,21 @@ fn rt_simulation(ts: &Transys, sim: &mut VarMap<BitVec>, nr_patt: usize) {
             block.push(!na);
         }
         slv.add_clause(&block);
-        dfs(ts, sim, slv, consider, domain, nr_patt, sim[Var::CONST].len() - 1);
+        dfs(
+            ts,
+            sim,
+            slv,
+            consider,
+            domain,
+            nr_patt,
+            sim[Var::CONST].len() - 1,
+        );
     }
 
     for from in 0..init_len {
-        if sim[Var::CONST].len() >= nr_patt { return; }
+        if sim[Var::CONST].len() >= nr_patt {
+            return;
+        }
         dfs(ts, &mut *sim, &mut slv, &consider, &domain, nr_patt, from);
     }
 }
